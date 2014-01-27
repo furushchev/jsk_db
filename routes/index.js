4e65ff930672794e39aa39af116c6b6861b58c24
garaemon <garaemon@gmail.com>
2014-01-27 18:18:03 +0900

    #1: adding new route: interactive_markr_test/json

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var _ = require('lodash');
-var files = ['_index', 'interactive_marker_test'];
+var files = ['_index', 'interactive_marker_test', 'interactive_marker_test_json'];
 
 exports.routes = _.map(files, function(f) {
     return require('./' + f);
